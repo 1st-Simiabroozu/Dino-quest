3b1a3c94d372760ca03abd723d90ecebe3786b5f
1st_Simiabroozu <137213728+1st-Simiabroozu@users.noreply.github.com>
2024-11-19 02:04:26 +0100

    Update skin.h

--- projet info/skin.h
+++ projet info/skin.h
@@ -1,3 +1,3 @@
 enum Player_class{Paladin,Druid,Berserker}; 
-void init_character(int row, int col, char board[row][col], enum Player_class player);
+void init_character(int row, int col, char board[row][col], enum Player_class player, int pos,  int height);
 
